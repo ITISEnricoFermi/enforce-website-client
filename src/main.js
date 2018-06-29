481778c014368892202e8fac9af8ca8989b9da21
Riccardo Sangiorgio <richard.sangiorgio@gmail.com>
2018-06-29 13:12:03 +0000

    Modifiche socket.io

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import VueParticles from 'vue-particles'
 import App from './App'
 import router from './router'
 
-export const SocketInstance = socketio('/', {secure: true, rejectUnauthorized: false, transports: ['websocket', 'flashsocket', 'polling']})
+export const SocketInstance = socketio(window.location.hostname, {secure: true, rejectUnauthorized: false})
 
 export const eventBus = new Vue({
   methods: {
