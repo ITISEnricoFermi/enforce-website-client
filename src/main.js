f7003e2d4993fd08c8bface739685e4757ab33c0
Riccardo Sangiorgio <richard.sangiorgio@gmail.com>
2018-06-29 11:44:30 +0000

    Socket.io

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import VueParticles from 'vue-particles'
 import App from './App'
 import router from './router'
 
-export const SocketInstance = socketio('185.25.207.165:4000', {secure: true, rejectUnauthorized: false})
+export const SocketInstance = socketio(window.location.hostname, {secure: true, rejectUnauthorized: false})
 
 export const eventBus = new Vue({
   methods: {
